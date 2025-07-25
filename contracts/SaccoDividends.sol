43f41645e5e03f8f9f71f3d94316c0f0edeaef4b
Cosmas Ken <Cosmasarubaken@gmail.com>
2025-07-25 12:10:36 +0300

    remove sacco functionality

--- contracts/SaccoDividends.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-contract SaccoDividends {
-    // Dividend logic will go here
-    function distributeDividends() external {}
-    function getDividendBalance(address user) external view returns (uint256) {}
-}
